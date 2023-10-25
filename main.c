4611c0941236e21aec400b683855eabda2ab4a12
Max <max.robert@epitech.eu>
2023-10-25 14:37:37 +0200

    add : flag_e

---
+++ main.c
@@ -0,0 +1,14 @@
+/*
+** EPITECH PROJECT, 2023
+** B-CPE-101-LYN-1-1-myprintf-florian.reynaud
+** File description:
+** main
+*/
+
+#include "include/my.h"
+#include <stdio.h>
+
+int main()
+{
+    printf("D%e\n", 300000.0);
+}
