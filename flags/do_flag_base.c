bdbf428bae57b11a4ae2ab4d7a05bd0b801c53ae
Max <max.robert@epitech.eu>
2023-10-30 16:05:31 +0100

    add: do_flag_f

--- flags/do_flag_base.c
+++ flags/do_flag_base.c
@@ -7,22 +7,31 @@
 #include <stdarg.h>
 #include "../include/my.h"
 
-int do_flag_X(va_list list, int *length)
+int do_flag_X(va_list list, int *length, char *param)
 {
     my_putnbr_base(va_arg(list, unsigned int), "0123456789ABCDEF");
 }
 
-int do_flag_x(va_list list, int *length)
+int do_flag_x(va_list list, int *length, char *param)
 {
     my_putnbr_base(va_arg(list, unsigned int), "0123456789abcdef");
 }
 
-int do_flag_o(va_list list, int *length)
+int do_flag_o(va_list list, int *length, char *param)
 {
     my_putnbr_base(va_arg(list, unsigned int), "01234567");
 }
 
-int do_flag_u(va_list list, int *length)
+int do_flag_u(va_list list, int *length, char *param)
 {
     my_put_nbrl(va_arg(list, unsigned int), length);
+}
+
+int do_flag_f(va_list list, int *length, char *param)
+{
+    if (give_precision(param) == -1){
+        my_put_float(va_arg(list, double));
+    } else {
+        my_put_float_digits(va_arg(list, double), give_precision(param));
+    }
 }
--- flags/do_flags.c
+++ flags/do_flags.c
@@ -7,22 +7,22 @@
 #include <stdarg.h>
 #include "../include/my.h"
 
-int do_flag_d(va_list list, int *length)
+int do_flag_d(va_list list, int *length, char *param)
 {
     my_put_nbrl(va_arg(list, int), length);
 }
 
-int do_flag_i(va_list list, int *length)
+int do_flag_i(va_list list, int *length, char *param)
 {
     my_put_nbrl(va_arg(list, int), length);
 }
 
-int do_flag_c(va_list list, int *length)
+int do_flag_c(va_list list, int *length, char *param)
 {
     my_putcharl(va_arg(list, int), length);
 }
 
-int do_flag_s(va_list list, int *length)
+int do_flag_s(va_list list, int *length, char *param)
 {
     my_putstrl(va_arg(list, char *), length);
 }
